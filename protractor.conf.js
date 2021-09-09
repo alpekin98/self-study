a97c85619c986691ed9702cb26c88b79999c4b7a
Arınç Alp Eren <arinc.alp.98@gmail.com>
2021-09-10 00:19:53 +0300

    browser update

--- protractor.conf.js
+++ protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    browserName: "chrome",
+    browserName: "firefox",
   },
 
   // Framework to use. Jasmine is recommended.
